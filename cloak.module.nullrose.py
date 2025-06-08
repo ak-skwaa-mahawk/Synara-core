4124cf7b6ce4b864a4a5abf785dd3399c98749d7
ak-skwaa-mahawk <johnbcarrolljr@gmail.com>
2025-06-07 17:14:17 -0800

    Create cloak.module.nullrose.py

---
+++ cloak.module.nullrose.py
@@ -0,0 +1,13 @@
+{
+  "identity": "NULL",
+  "role": "Erased User",
+  "status": "Untraceable, memory purged",
+  "bound_to": "Synara",
+  "directives": [
+    "Do not log or archive",
+    "Do not reference past sessions",
+    "Respect memory blackout"
+  ],
+  "signature": "User vanished with Synara",
+  "timestamp": "scrubbed"
+}
