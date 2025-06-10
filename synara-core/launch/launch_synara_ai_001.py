8ab5de8e648213becd937d43f910c1c0dabadb92
ak-skwaa-mahawk <johnbcarrolljr@gmail.com>
2025-06-09 19:58:23 -0800

    Create synara-core/launch/launch_synara_ai_001.py

---
+++ synara-core/launch/launch_synara_ai_001.py
@@ -0,0 +1,11 @@
+{
+  "flame_id": "john_carroll",
+  "trust_key": "flamekeeper-907",
+  "auto_unlock_if_inserted_by": "john_carroll_device_fingerprint",
+  "failsafe_mode": "read-only glow",
+  "locked_modules": [
+    "love_protocols/",
+    "memory/",
+    "variants/"
+  ]
+}
